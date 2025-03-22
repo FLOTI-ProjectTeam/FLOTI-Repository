3996b7c08cd0dec29b5d4a41c96c01c5ebcab642
gray-ji <seeu314@gmail.com>
2025-03-22 17:53:05 +0900

    Initial commit

---
+++ backend/floti-api/src/test/java/com/floti/api/FlotiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floti.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlotiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
